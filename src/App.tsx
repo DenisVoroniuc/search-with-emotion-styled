1d29af6081a70d17a2c16a9bfbb5087d0cdcdc92
DenisVoroniuc <dvoroniuc.themefuse@gmail.com>
2021-06-11 21:30:44 +0300

    created search using react js and emotion ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DATASET } from "./data";
+import styled from "@emotion/styled";
 
-function App() {
+const Content = styled.div`
+  font-family: sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ListWrapper = styled.ul`
+margin:15px
+display: flex;
+align-items: center;
+justify-content: center;
+padding: 0;
+`;
+
+const ListItem = styled.li`
+  background-color: #e1e4e8;
+  list-style-type: none;
+  flex: 1;
+  padding: 3px;
+  margin: 5px;
+  text-align: center;
+
+`;
+
+const SearchInput = styled.input`
+  width: 40rem;
+  padding: 16px 16px 16px;
+  border: none;
+  font-size: 20px;
+  background: rgb(255, 255, 255);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const SearchHeading = styled.h2`
+  text-align: center;
+  display: block;
+  margin-bottom: 50px;
+`;
+export const App = () => {
+  const [search, onSearch] = React.useState<string>("");
+  const [data, onSearchData] = React.useState<undefined | string[]>(undefined);
+
+  React.useEffect(() => {
+    onSearchData(DATASET.filter(item => item.includes(search)));
+  }, [search, onSearchData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content>
+      <div>
+        <SearchHeading>Search</SearchHeading>
+        <SearchInput type="text" value={search} onChange={e => onSearch(e.target.value.trim())} />
+        <ListWrapper>
+          {search.length ? data?.map((text, key) => <ListItem key={key}> {text}</ListItem>) : null}
+        </ListWrapper>
+      </div>
+    </Content>
   );
-}
-
-export default App;
+};
